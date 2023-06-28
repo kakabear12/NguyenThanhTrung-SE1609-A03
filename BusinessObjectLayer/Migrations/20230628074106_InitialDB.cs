9aafca159f131a9dc63b3f31d88554dbd239b76a
trungnt <trung2kgl@gmail.com>
2023-06-28 14:42:16 +0700

    Fix something

---
+++ BusinessObjectLayer/Migrations/20230628074106_InitialDB.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BusinessObjectLayer.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryName = table.Column<string>(type: "text", nullable: true),
+                    CategoryDescription = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    CustomerName = table.Column<string>(type: "text", nullable: true),
+                    City = table.Column<string>(type: "text", nullable: true),
+                    Country = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    Birthday = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    SupplierId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SupplierName = table.Column<string>(type: "text", nullable: true),
+                    SupplierAddress = table.Column<string>(type: "text", nullable: true),
+                    Telephone = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.SupplierId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CustomerId = table.Column<int>(type: "integer", nullable: true),
+                    OrderDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    ShippedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    Total = table.Column<decimal>(type: "numeric", nullable: true),
+                    OrderStatus = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlowerBouquets",
+                columns: table => new
+                {
+                    FlowerBouquetId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false),
+                    FlowerBouquetName = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    UnitPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    UnitsInStock = table.Column<int>(type: "integer", nullable: false),
+                    FlowerBouquetStatus = table.Column<byte>(type: "smallint", nullable: true),
+                    SupplierId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlowerBouquets", x => x.FlowerBouquetId);
+                    table.ForeignKey(
+                        name: "FK_FlowerBouquets_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FlowerBouquets_Suppliers_SupplierId",
+                        column: x => x.SupplierId,
+                        principalTable: "Suppliers",
+                        principalColumn: "SupplierId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "integer", nullable: false),
+                    FlowerBouquetId = table.Column<int>(type: "integer", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "numeric", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Discount = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => new { x.OrderId, x.FlowerBouquetId });
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_FlowerBouquets_FlowerBouquetId",
+                        column: x => x.FlowerBouquetId,
+                        principalTable: "FlowerBouquets",
+                        principalColumn: "FlowerBouquetId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlowerBouquets_CategoryId",
+                table: "FlowerBouquets",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlowerBouquets_SupplierId",
+                table: "FlowerBouquets",
+                column: "SupplierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_FlowerBouquetId",
+                table: "OrderDetails",
+                column: "FlowerBouquetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "FlowerBouquets");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
--- BusinessObjectLayer/Models/FUFlowerBouquetManagementContext.cs
+++ BusinessObjectLayer/Models/FUFlowerBouquetManagementContext.cs
@@ -29,175 +29,22 @@ namespace BusinessObjectLayer.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var builder = new ConfigurationBuilder()
-           .SetBasePath(Directory.GetCurrentDirectory())
-           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
             IConfigurationRoot configuration = builder.Build();
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyDB"));
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("MyDB"));
         }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
-
-            modelBuilder.Entity<Category>(entity =>
-            {
-                entity.ToTable("Category");
-
-                entity.Property(e => e.CategoryId)
-                    .ValueGeneratedNever()
-                    .HasColumnName("CategoryID");
-
-                entity.Property(e => e.CategoryDescription).HasMaxLength(150);
-
-                entity.Property(e => e.CategoryName)
-                    .IsRequired()
-                    .HasMaxLength(40)
-                    .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<Customer>(entity =>
-            {
-                entity.ToTable("Customer");
-
-                entity.Property(e => e.CustomerId)
-                    .ValueGeneratedNever()
-                    .HasColumnName("CustomerID");
-
-                entity.Property(e => e.Birthday).HasColumnType("date");
-
-                entity.Property(e => e.City)
-                    .IsRequired()
-                    .HasMaxLength(15)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Country)
-                    .IsRequired()
-                    .HasMaxLength(15)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.CustomerName)
-                    .IsRequired()
-                    .HasMaxLength(180)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Email)
-                    .IsRequired()
-                    .HasMaxLength(100)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Password)
-                    .IsRequired()
-                    .HasMaxLength(30)
-                    .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<FlowerBouquet>(entity =>
-            {
-                entity.ToTable("FlowerBouquet");
-
-                entity.Property(e => e.FlowerBouquetId)
-                    .ValueGeneratedNever()
-                    .HasColumnName("FlowerBouquetID");
-
-                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
-
-                entity.Property(e => e.Description)
-                    .IsRequired()
-                    .HasMaxLength(220)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.FlowerBouquetName)
-                    .IsRequired()
-                    .HasMaxLength(40)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
-
-                entity.Property(e => e.UnitPrice).HasColumnType("money");
-
-                entity.HasOne(d => d.Category)
-                    .WithMany(p => p.FlowerBouquets)
-                    .HasForeignKey(d => d.CategoryId)
-                    .HasConstraintName("FK__FlowerBou__Categ__412EB0B6");
-
-                entity.HasOne(d => d.Supplier)
-                    .WithMany(p => p.FlowerBouquets)
-                    .HasForeignKey(d => d.SupplierId)
-                    .OnDelete(DeleteBehavior.Cascade)
-                    .HasConstraintName("FK__FlowerBou__Suppl__4222D4EF");
-            });
-
-            modelBuilder.Entity<Order>(entity =>
-            {
-                entity.ToTable("Order");
-
-                entity.Property(e => e.OrderId)
-                    .ValueGeneratedNever()
-                    .HasColumnName("OrderID");
-
-                entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
-
-                entity.Property(e => e.OrderDate).HasColumnType("datetime");
-
-                entity.Property(e => e.OrderStatus)
-                    .HasMaxLength(10)
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.ShippedDate).HasColumnType("datetime");
-
-                entity.Property(e => e.Total).HasColumnType("money");
-
-                entity.HasOne(d => d.Customer)
-                    .WithMany(p => p.Orders)
-                    .HasForeignKey(d => d.CustomerId)
-                    .OnDelete(DeleteBehavior.Cascade)
-                    .HasConstraintName("FK__Order__CustomerI__4316F928");
-            });
 
             modelBuilder.Entity<OrderDetail>(entity =>
             {
-                entity.HasKey(e => new { e.OrderId, e.FlowerBouquetId })
-                    .HasName("PK__OrderDet__C983CA9F0EAA733D");
-
-                entity.ToTable("OrderDetail");
-
-                entity.Property(e => e.OrderId).HasColumnName("OrderID");
-
-                entity.Property(e => e.FlowerBouquetId).HasColumnName("FlowerBouquetID");
-
-                entity.Property(e => e.UnitPrice).HasColumnType("money");
-
-                entity.HasOne(d => d.FlowerBouquet)
-                    .WithMany(p => p.OrderDetails)
-                    .HasForeignKey(d => d.FlowerBouquetId)
-                    .HasConstraintName("FK__OrderDeta__Flowe__440B1D61");
-
-                entity.HasOne(d => d.Order)
-                    .WithMany(p => p.OrderDetails)
-                    .HasForeignKey(d => d.OrderId)
-                    .HasConstraintName("FK__OrderDeta__Order__44FF419A");
+                entity.HasKey(e => new { e.OrderId, e.FlowerBouquetId });
             });
 
-            modelBuilder.Entity<Supplier>(entity =>
-            {
-                entity.ToTable("Supplier");
-
-                entity.Property(e => e.SupplierId)
-                    .ValueGeneratedNever()
-                    .HasColumnName("SupplierID");
-
-                entity.Property(e => e.SupplierAddress).HasMaxLength(150);
-
-                entity.Property(e => e.SupplierName).HasMaxLength(50);
-
-                entity.Property(e => e.Telephone)
-                    .HasMaxLength(15)
-                    .IsFixedLength(true);
-            });
 
             OnModelCreatingPartial(modelBuilder);
         }
-
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
